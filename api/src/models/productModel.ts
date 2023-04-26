4190917cd1b856481d5e5dd620328260eb6f030d
ヾ(≧∇≦*)ゝ <krisdanoda@users.noreply.github.com>
2023-04-26 12:44:12 +0200

    Update productModel.ts

---
+++ api/src/models/productModel.ts
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String,
+        required: false
+    }
+});
+
+const MongooseModel = mongoose.model('MongooseModel', productSchema);
+
+module.exports = MongooseModel;
