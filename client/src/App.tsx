0333eb61132fe6979e84b5fa05cd66cfc1fc993d
SMerved <90383268+SMerved@users.noreply.github.com>
2023-05-01 14:59:06 +0200

    push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import ShopPage from './pages/ShopPage'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4001/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
 
   return (
+    <ApolloProvider client={client}>
     <ShopPage/>
+    </ApolloProvider>
   )
   
 }
--- client/src/pages/ShopPage.tsx
+++ client/src/pages/ShopPage.tsx
@@ -1,14 +1,17 @@
 import {Grid} from "@mui/material"
+import { useQuery } from "@apollo/client/react";
 import ProductCard from "../components/ProductCard"
+import GetAllProducts from "../resolvers/queries/GqlGetAllProducts";
 
 function ShopPage() {
   const products = [1,2,3,4,5,6,7]
-  /*const { loading, error, data } = useQuery(GetAllProducts);
+  const { loading, error, data } = useQuery(GetAllProducts);
 
 if (loading) return <p>Loading...</p>;
-if (error) return <p>Error : {error.message}</p>;*/
+if (error) return <p>Error : {error.message}</p>;
 
   return (
+    <div>
     <div id={'shop_outer_div'}>
       <Grid container id={'grid_container_shop'} gap={10} spacing={1}>
         {products.map((product)=>
@@ -17,6 +20,7 @@ if (error) return <p>Error : {error.message}</p>;*/
         </Grid>
         )}
       </Grid>
+    </div>
     </div>
   )
 }
