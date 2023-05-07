5f354e7dd1cda31283eb4926cf8769c117962d26
ヾ(≧∇≦*)ゝ <krisdanoda@users.noreply.github.com>
2023-05-07 13:25:17 +0200

    Refetch actually works

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,47 @@
-import { useState , useEffect, useReducer} from 'react'
+import {useState, useEffect, useReducer} from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './pages/HomePage'
 import ShopPage from './pages/ShopPage'
-import { usePageTitle } from './usePageTitle';
+import {usePageTitle} from './usePageTitle';
 import Footer from './components/Footer';
 import CartPage from './pages/CartPage';
 import ProductPage from './pages/ProductPage';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import {ApolloClient, InMemoryCache, ApolloProvider, gql} from '@apollo/client';
 import CheckOutPage from './pages/CheckOutPage';
-import { CartContextProvider } from './contexts/CartContext';
+import {CartContextProvider} from './contexts/CartContext';
+import AdminPage from "./pages/AdminPage";
 
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4001/graphql',
-  cache: new InMemoryCache(),
+    uri: 'http://localhost:4001/graphql',
+    cache: new InMemoryCache(),
 });
 
 function App() {
-  const currentPage = usePageTitle();
-  return (
-    <ApolloProvider client={client}>
-      <CartContextProvider>
-      <div>
-        <NavBar page={currentPage}/>
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/shop" element={<ShopPage/>}/>
-          <Route path="/product/:productId" element={<ProductPage/>} />
-          <Route path="/cart" element={<CartPage/>} />
-          <Route path='/checkout' element={<CheckOutPage/>}/>
-          <Route path="*" element={<h1>Page Not Found !!!!</h1>}/>
-        </Routes>
-        <Footer/>
-      </div>
-      </CartContextProvider>
-    </ApolloProvider>
+    const currentPage = usePageTitle();
+    return (
+        <ApolloProvider client={client}>
+            <CartContextProvider>
+                <div>
+                    <NavBar page={currentPage}/>
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/shop" element={<ShopPage/>}/>
+                        <Route path="/product/:productId" element={<ProductPage/>}/>
+                        <Route path="/cart" element={<CartPage/>}/>
+                        <Route path='/checkout' element={<CheckOutPage/>}/>
+                        <Route path="/adminPage" element={<AdminPage/>}></Route>
+                        <Route path="*" element={<h1>Page Not Found !!!!</h1>}/>
+                    </Routes>
+                    <Footer/>
+                </div>
+            </CartContextProvider>
+        </ApolloProvider>
 
-  )
-  
+    )
+
 }
 
 export default App
--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -1,9 +1,14 @@
+import CreateProduct from "../components/CreateProduct";
+import EditProducts from "../components/EditProducts";
+import {useEffect, useState} from "react";
+
 function AdminPage() {
-    
-  
+    const [count, setCount] = useState();
+
     return (
       <div>
-        
+          <CreateProduct></CreateProduct>
+          <EditProducts></EditProducts>
       </div>
     )
   }
