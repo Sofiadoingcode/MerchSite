fe38a927add6174f433f0c6e7ffabcdbeb48c88a
Magical Glove <90389752+MagicalGlove@users.noreply.github.com>
2023-05-08 13:04:03 +0200

    Can now edit

--- client/src/components/AdminPage/EditProduct.tsx
+++ client/src/components/AdminPage/EditProduct.tsx
@@ -6,17 +6,34 @@ import {Button, Card} from "@mui/material";
 import '../../styles/editproduct.css'
 
 const EditProduct = (props: { product: Product }) => {
-    const [editProduct, {loading, error}] = useMutation(GqlEditProduct);
 
-    const handleSubmit = (event: FormEvent<T>) => {
+    const [editProduct, {loading, error}] = useMutation(GqlEditProduct);
+    const [updateProduct, setUpdateProduct] = useState<Product>({
+        id: props.product.id,
+        name: props.product.name,
+        description: props.product.description,
+        price: props.product.price,
+        category: props.product.category,
+        size: props.product.size,
+    });
+
+    const handleInputChange = (event: any) => {
+        const {name, value} = event.target;
+
+        if (name === 'price') {
+            setUpdateProduct({...updateProduct, [name]: parseFloat(value)});
+        } else {
+            setUpdateProduct({...updateProduct, [name]: value});
+        }
+    };
+
+    const handleSubmit = (event: any) => {
         event.preventDefault();
-        console.log("About To Edit!")
-        console.log("I'm a prop product: " + props.product.id)
         editProduct({
             variables: {
-                id: props.product.id,
+                id: updateProduct.id,
                 input: {
-                    ...props.product,
+                    ...updateProduct,
                 },
             },
         })
@@ -30,10 +47,15 @@ const EditProduct = (props: { product: Product }) => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <Button variant="contained" type="submit" disabled={loading}>
+            <td><input type="string" name="name" defaultValue={props.product.name} onChange={handleInputChange}/></td>
+            <td><input type="string" name="description" defaultValue={props.product.description} onChange={handleInputChange}/></td>
+            <td><input type="number" name="price" defaultValue={props.product.price} onChange={handleInputChange}/></td>
+            <td><input type="string" name="category" defaultValue={props.product.category} onChange={handleInputChange}/></td>
+            <td><input type="string" name= "size" defaultValue={props.product.size} onChange={handleInputChange}/></td>
+            <td><Button variant="contained" type="submit" disabled={loading}>
                 {loading ? "Updating..." : "Update Product"}
             </Button>
-            {error && <p>Error updating product</p>}
+                {error && <p>Error updating product</p>}</td>
         </form>
     );
 }
--- client/src/components/EditProducts.tsx
+++ client/src/components/EditProducts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {useMutation} from "@apollo/client";
 import GqlCreateProduct from "../resolvers/mutations/GqlCreateProduct";
@@ -7,11 +7,17 @@ import {useQuery} from "@apollo/client/react";
 import GetAllProducts from "../resolvers/queries/GqlGetAllProducts";
 import EditProduct from './AdminPage/EditProduct';
 import DeleteProduct from './AdminPage/DeleteProduct';
+import {Product} from "../types";
 
 function EditProducts() {
     const {loading, error, data} = useQuery(GetAllProducts);
+
+   
+
     if (loading) return <>'Submitting...'</>;
     if (error) return <>`Submission error! ${error.message}`</>;
+
+
 
     return (
 
@@ -27,13 +33,8 @@ function EditProducts() {
                     <th>Edit</th>
                     <th>Delete</th>
                 </tr>
-                {data.products.map((product: any) => (<tr>
+                {data.products.map((product: Product) => (<tr>
                     <td>  {product.id}  </td>
-                    <td> <input type = "string" defaultValue={product.name} /> </td>
-                    <td> <input type = "string" defaultValue={product.description} /> </td>
-                    <td> <input type = "number" defaultValue={product.price} /> </td>
-                    <td> <input type = "string" defaultValue={product.category} /> </td>
-                    <td> <input type = "string" defaultValue={product.size} /> </td>
                     <td> <EditProduct product={product}/></td>
                     <td> <button> <DeleteProduct productID={product.id}/> </button></td>
                 </tr>))}
